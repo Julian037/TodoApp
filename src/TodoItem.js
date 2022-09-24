5ebb662aa8fa702ec21aabddb017042e3e4996ff
julian037 <jv.julian1313@gmail.com>
2022-09-24 09:41:44 -0500

    se agrega funcionalidad en boton para completar una tarea y en el boton de eliminar la misma

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,19 +3,11 @@ import './TodoItem.css'
 
 function TodoItem(props){
 
-    const onComplete = () => {
-        alert('completaste el todo')
-    };
-
-    const onDelete = () => {
-        alert('eliminaste el todo')
-    };
-
     return(
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`} onClick={onComplete}>√</span>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`} onClick={props.onComplete}>√</span>
                 <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete" onClick={onDelete}>X</span>
+            <span className="Icon Icon-delete" onClick={props.onDelete}>X</span>
         </li>
     )
 }
