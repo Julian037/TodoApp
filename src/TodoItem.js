db6214adf66c865be3315af1c3f5fa30c948e701
julian037 <jv.julian1313@gmail.com>
2022-09-24 20:44:18 -0500

    se agreg font awesome

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,19 @@
 import React from "react";
 import './TodoItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+
 
 function TodoItem(props){
 
+    const trash = <FontAwesomeIcon icon={faTrash} />
+    const check = <FontAwesomeIcon icon={faCircleCheck} />
+
     return(
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`} onClick={props.onComplete}>√</span>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`} onClick={props.onComplete}>{check}√</span>
                 <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete" onClick={props.onDelete}>X</span>
+            <span className="Icon Icon-delete" onClick={props.onDelete}>{trash}X</span>            
         </li>
     )
 }
